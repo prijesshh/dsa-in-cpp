c8647a98cb40489d968cc94997d6eff11d232baf
Prijesh Pradhan <prijeshpradhan@gmail.com>
2026-05-14 22:13:49 +0545

    Initial commit

---
+++ 1-Recursion/8-factorial.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int fact(int n)
+{
+    if (n == 0)
+        return 1;
+    else
+        return n * fact(n - 1);
+}
+int main()
+{
+    cout << "Factorial is:" << fact(5);
+}
