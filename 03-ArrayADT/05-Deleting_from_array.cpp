02ac45be5fb1d65c60bf8e47ee6c3161fb233593
Prijesh Pradhan <prijeshpradhan@gmail.com>
2026-05-24 18:24:23 +0545

    Initial commit

---
+++ 03-ArrayADT/05-Deleting_from_array.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+void display(int arr[], int length)
+{
+
+    for (int i = 0; i < length; i++)
+    {
+        cout << arr[i];
+    }
+    cout << "\n";
+}
+int main()
+{
+    int arr[] = {1, 2, 9, 3, 4, 5};
+    int length = 6;
+    display(arr, length);
+    // to delet element of index 2
+    //first check if index lies in our array or not
+    if(index<length)
+    {
+    int index = 2;
+    for (int i = index; i < length - 1; i++)
+    {
+        arr[i] = arr[i + 1];
+    }
+    length--;
+    display(arr, length);
+}
+else
+cout<<"Index out of range";
+}
+//time complexity is O(n)
