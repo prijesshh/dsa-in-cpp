748148337b7dbbb198987d369a6f648f0ebf56a8
Prijesh Pradhan <prijeshpradhan@gmail.com>
2026-06-07 21:07:50 +0545

    duplicate element

---
+++ 03-ArrayADT/23-Finding_duplicate_elements.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int A[] = {3, 6, 8, 8, 10, 12, 15, 15, 15, 15, 20};
+    int n = sizeof(A) / sizeof(A[0]);
+    for (int i = 0; i < n - 1; i++) // i<n-1 bcz i+1 must always be valid
+    {
+        if (A[i] == A[i + 1])
+        {
+            int j = i + 1; // to count
+
+            while (j < n && A[j] == A[i])
+                j++;
+
+            cout << A[i] << " is duplicate " << (j - i) << " times" << endl;
+
+            i = j;
+        }
+    }
+}
